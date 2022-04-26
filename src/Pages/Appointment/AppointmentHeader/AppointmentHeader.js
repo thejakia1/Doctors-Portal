974512c2916fc12384e75c3143300eb913e19324
thejakia1 <thejakia1@gmail.com>
2022-04-27 05:10:00 +0600

    shared calender started

--- src/Pages/Appointment/AppointmentHeader/AppointmentHeader.js
+++ src/Pages/Appointment/AppointmentHeader/AppointmentHeader.js
@@ -1,13 +1,14 @@
 import { Container, Grid } from '@mui/material';
 import React from 'react';
 import chair from '../../../images/chair.png'
+import Calender from '../../Shared/Calender/Calender';
 
 const AppointmentHeader = () => {
     return (
         <Container>
             <Grid container spacing={2}>
                 <Grid item xs={12} md={6}>
-
+                    <Calender></Calender>
                 </Grid>
                 <Grid item xs={12} md={6}>
                     <img style={{ width: "100%" }} src={chair} alt="" />
---
+++ src/Pages/Shared/Calender/Calender.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import isWeekend from 'date-fns/isWeekend';
+import TextField from '@mui/material/TextField';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
+
+const Calender = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Calender;
