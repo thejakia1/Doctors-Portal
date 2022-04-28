9fa8b03027ae2e323665fdd1830b5850b345df15
thejakia1 <thejakia1@gmail.com>
2022-04-29 05:08:43 +0600

    firebase setup

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -4,20 +4,38 @@ import login from "../../../images/login.png";
 import { NavLink } from "react-router-dom";
 
 const Register = () => {
+  const [loginData, setLoginData] = useState({});
+
+  const handleOnChange = (e) => {
+    const field = e.target.name;
+    const value = e.target.value;
+    const newLoginData = { ...loginData };
+    newLoginData[field] = value;
+    setLoginData(newLoginData);
+  };
+  const handleLoginSubmit = (e) => {
+    if(loginData.password != loginData.password2){
+      alert("Your password did not match");
+      return; 
+    }
+    e.preventDefault();
+  };
+
   return (
     <Container>
       <Grid container spacing={2}>
         <Grid item sx={{ mt: 20 }} xs={12} md={6}>
           <Typography variant="body2" gutterBottom>
-            Login
+            Register
           </Typography>
-          <form>
+          <form onSubmit={handleLoginSubmit}>
             <TextField
               sx={{ width: "75%", m: 1 }}
               id="standard-basic"
               label="Your Email"
               name="email"
-              // onChange={handleOnChange}
+              type="email"
+              onChange={handleOnChange}
               variant="standard"
             />
             <TextField
@@ -25,6 +43,14 @@ const Register = () => {
               id="standard-basic"
               label="Your Password"
               name="password"
+              type="password"
+              variant="standard"
+            />
+            <TextField
+              sx={{ width: "75%", m: 1 }}
+              id="standard-basic"
+              label="Re-type Your Password"
+              name="password2"
               type="password"
               variant="standard"
             />
