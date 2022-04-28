8d6ffa1587158cc4d09cab7b3fb98112731554cc
thejakia1 <thejakia1@gmail.com>
2022-04-28 20:48:08 +0600

    login page img

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
           <Route path="/home">
             <Home />
           </Route>
-          <Route path="/">
+          <Route exact path="/">
             <Home />
           </Route>
         </Switch>
